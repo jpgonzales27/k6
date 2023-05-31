2272a320e67a95085f052f8f4420942f6300fc3c
Juan Gonzales Alvarado <Juan.Gonzales@jalasoft.com>
2023-05-31 17:51:33 -0400

    check thredsholds reto2

--- 12-check-thresholds-reto2.js
+++ 12-check-thresholds-reto2.js
@@ -1,30 +1,35 @@
 import http from "k6/http";
-import { Rate } from 'k6/metrics';
+import { Rate } from "k6/metrics";
 
 export const options = {
   vus: 10,
-  duration: '20s',
+  duration: "20s",
   thresholds: {
     http_req_failed: ["rate<0.35"],
-    http_req_failed: [{
-      threshold: "rate<0.35",
-      abortonFail: true,
-      delayAbortEval: "10s"
-    }]
-  }
-}
+    http_req_failed: [
+      {
+        threshold: "rate<0.35",
+        abortonFail: true,
+        delayAbortEval: "10s",
+      },
+    ],
+  },
+};
 
 const rate = new Rate("exists_product");
 
-export default function(){
-  const randomProduct = random(1, 300);// Return random value between 1 and 300
+export default function () {
+  const randomProduct = random(1, 300); // Return random value between 1 and 300
   const response = http.get("https://api.escuelajs.co/api/v1/products/" + String(randomProduct));
 
   // If the response status equals 400
-  if(response.status === 400){
+  if (response.status === 400) {
     rate.add(0);
-  }
-  else{
+  } else {
     rate.add(1);
   }
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
